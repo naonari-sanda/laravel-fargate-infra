a168fe71e18bc40d2448b2d91945e06ad6f2ec2c
Nonari Sanda <mkg_sandy@icloud.com>
2022-02-08 22:49:15 +0900

    test commit

--- envs/prod/routing/aws-tripsupporter_com/backend.tf
+++ envs/prod/routing/aws-tripsupporter_com/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "naonari-tfstate"
-    key    = "example/prod/routing/appfoobar_link_v1.0.0.tfstate"
+    key    = "example/prod/routing/aws-tripsupporter_v1.0.0.tfstate"
     region = "ap-northeast-1"
   }
 }
