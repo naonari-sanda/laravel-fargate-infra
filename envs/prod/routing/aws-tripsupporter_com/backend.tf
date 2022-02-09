fe78901739767ad0c39f4a45e2c93773df092948
Nonari Sanda <mkg_sandy@icloud.com>
2022-02-09 22:23:06 +0900

    fix backend.tf

--- envs/prod/routing/aws-tripsupporter_com/backend.tf
+++ envs/prod/routing/aws-tripsupporter_com/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "naonari-tfstate"
-    key    = "example/prod/routing/aws-tripsupporter_v1.0.0.tfstate"
+    key    = "example/prod/routing/aws-tripsupporter_com_v1.0.0.tfstate"
     region = "ap-northeast-1"
   }
 }
